f45c1eacea93b8c18929aead08a4dfce7956e9d7
mhdxo-66 <mahdiarvandi99@gmail.com>
2026-09-09 08:50:44 +0330

    Added Score Backend

--- Sources/scrtips.js
+++ Sources/scrtips.js
@@ -3,3 +3,9 @@ let player1 = 0;
 let player2 = 0;
 let pl1 = document.querySelector('.pl1');
 let pl2 = document.querySelector('.pl2');
+
+function plscore() {
+    pl1.textContent = player1;
+    pl2.textContent = player2;
+}
+plscore();
