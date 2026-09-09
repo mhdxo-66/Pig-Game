8759e8da737572de3056f85f1ee7c08d29c1749d
mhdxo-66 <mahdiarvandi99@gmail.com>
2026-09-09 09:14:23 +0330

    Fixed Import PLayer Score

--- Sources/scrtips.js
+++ Sources/scrtips.js
@@ -1,11 +1,16 @@
 "use strict";
-let player1 = 0;
-let player2 = 0;
-let pl1 = document.querySelector('.pl1');
-let pl2 = document.querySelector('.pl2');
-
+let pl1 = 0;
+let pl2 = 0;
+let player1 = document.querySelector('.pl1');
+let player2 = document.querySelector('.pl2');
+let current1 = 0;
+let current2 = 0;
+let c1 = document.querySelector('.current1');
+let c2 = document.querySelector('.current2');
 function plscore() {
-    pl1.textContent = player1;
-    pl2.textContent = player2;
-}
+    player1.textContent = pl1;
+    player2.textContent = pl2;
+    current1.textContent = c1;
+    current2.textContent = c2;
+};
 plscore();
