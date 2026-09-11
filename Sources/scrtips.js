e7e01cc3db4d3ff52fe534b8f61eb15f83d75eda
mhdxo-66 <mahdiarvandi99@gmail.com>
2026-09-11 10:00:00 +0330

    Fixed Height And Padding

--- Sources/scrtips.js
+++ Sources/scrtips.js
@@ -10,7 +10,7 @@ let c2 = document.querySelector('.current2');
 function plscore() {
     player1.textContent = pl1;
     player2.textContent = pl2;
-    c1.textContent = current1;
-    c2.textContent = current2;
+    c1.innerHTML = `Current <br> - ${current1} -`;
+    c2.innerHTML = `Current <br> - ${current2} -`;
 };
 plscore();
