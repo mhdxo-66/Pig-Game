21d7864e40bd16e7073bdbd69882718138e896f9
mhdxo-66 <mahdiarvandi99@gmail.com>
2026-09-14 11:00:00 +0330

    Added Phisycal Dise Stat

--- Sources/scrtips.js
+++ Sources/scrtips.js
@@ -1,12 +1,13 @@
 "use strict";
 let pl1 = JSON.parse(localStorage.getItem('pl1')) || 0;
-let pl2 = JSON.parse(localStorage.getItem('pl2')) || -19;
+let pl2 = JSON.parse(localStorage.getItem('pl2')) || 0;
 let player1 = document.querySelector('.pl1');
 let player2 = document.querySelector('.pl2');
 let current1 = JSON.parse(localStorage.getItem('current1')) || 0;
 let current2 = JSON.parse(localStorage.getItem('current2')) || 0;
 let c1 = document.querySelector('.current1');
 let c2 = document.querySelector('.current2');
+let ds = document.querySelector('.ds');
 function plscore() {
     player1.textContent = pl1;
     player2.textContent = pl2;
@@ -18,10 +19,10 @@ let p1 = true;
 let p2 = false;
 
 function gover() {
-    if (pl1 <= 20) {
+    if (pl1 <= -20) {
         alert(`Player 2 Won`)
         rst();
-    } else if (pl2 <= 20) {
+    } else if (pl2 <= -20) {
         alert(`Player 1 Won`)
         rst();
     }
@@ -35,8 +36,8 @@ document.querySelector('.rst').addEventListener('click', function () {
     rst();
 })
 function rnmc() {
-    // return Math.trunc(Math.random() * 6) + 1;
-    return 1;
+    let rnm = Math.trunc(Math.random() * 6) + 1;
+    return rnm;
 }
 let clr1 = document.querySelector('.P1');
 let clr2 = document.querySelector('.P2');
@@ -47,6 +48,7 @@ function clrc(stat) {
 clrc(1);
 document.querySelector('.dise').addEventListener('click', function () {
     let rnum = rnmc();
+    ds.innerHTML = `<img src="./dice-${rnum}.png" class="dsst">`;
     console.log(rnum);
     if (p1 == true) {
         if (rnum > 1) {
